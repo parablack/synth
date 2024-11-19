b5a793b9bec9577316e30d82078d679e6dc60f4d
Simon Schwarz <simonschwarz.du@gmail.com>
2024-11-19 11:45:43 +0100

    Make this package installable, and fix some bugs in the example

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='synth',
+    version='1.0',
+    author='Sebastian Hack, Nicolas Edelmann',
+    author_email='hack@cs.uni-saarland.de',
+    description='Synthesis of Loop-free Programs',
+    packages=find_packages(),
+)
